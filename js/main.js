02aedcc3a3e2368ead2007bf636499e03b942076
nermeen213 <nermeeen1734@gmail.com>
2024-01-12 02:13:35 +0200

    three commit

--- js/main.js
+++ js/main.js
@@ -32,12 +32,12 @@ for(let i=0 ; i<icon.length ; i++){
     icon[i].addEventListener('click',function(){
         document.body.classList.toggle('dark-theme')
         if(document.body.classList.contains('dark-theme')){
-            icon.classList.replace('fa-moon','fa-sun')
+            icon[i].classList.replace('fa-moon','fa-sun')
     
     
         }
         else{
-            icon.classList.replace('fa-sun','fa-moon')
+            icon[i].classList.replace('fa-sun','fa-moon')
     
     
         }
